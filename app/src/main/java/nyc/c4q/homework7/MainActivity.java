20ad8066815a7d2a694fe40f05b8c658d7972986
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2017-11-02 23:40:01 -0400

    logice only missing best score

--- app/src/main/java/nyc/c4q/homework7/MainActivity.java
+++ app/src/main/java/nyc/c4q/homework7/MainActivity.java
@@ -1,5 +1,4 @@
 package nyc.c4q.homework7;
-
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,50 +8,38 @@ import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
-
 public class MainActivity extends AppCompatActivity {
     private Button red, blue, green, yellow, play;
     private TextView score, bestScore, level;
     private Button[] color;
-    private boolean running ;
-    private int buttonTag = 1;
+    private boolean running;
+    private int buttonTag ;
     private int count = 1;
-
     AlphaAnimation animation = new AlphaAnimation(1f, 0f);
     int x;
     Random random;
     ArrayList<Integer> arrayListXvALUE;
     ArrayList<Integer> userChoiceList;
-
     Handler handler = new Handler();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setupview();
-
         //storage the color inside the button
         color = new Button[]{red, blue, green, yellow};
-
         //Generate Random number to access to the color
         random = new Random();
         //storge the color in the sequence to use them later .
         arrayListXvALUE = new ArrayList<>();
         //storge the user choice to compare later with arryListXValue;
         userChoiceList = new ArrayList<>();
-
         //to flash the button
         animation.setDuration(1000);
-
     }
-
 
     public void setupview() {
         red = (Button) findViewById(R.id.red);
@@ -69,108 +56,78 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onplay(final View view) {
-
-
         buttonTag = (int) play.getTag();
-
         if (buttonTag == 1) {
             play.setTag(2);
             play.setText("Quit game");
-               simon(view);
+            simon(view);
+            clickCheck(view);
+            score.setText("");
         } else if (buttonTag == 2) {
-
             play.setTag(1);
             play.setText("play");
-
+            arrayListXvALUE.clear();
+            score.setText("");
             Toast.makeText(getBaseContext(), "Game Finsh: ", Toast.LENGTH_SHORT).show();
+            // android.os.Process.killProcess(android.os.Process.myPid());
         }
     }
-
-
     private void simon(View view) {
-        running=true;
+        running = true;
         while (running) {
-
             x = random.nextInt(4);  //Generate Random number
-
             arrayListXvALUE.add(x);
+            Toast.makeText(this, "level number :"+arrayListXvALUE.size(), Toast.LENGTH_SHORT).show();
             Log.e("random number is", "" + x);  //Storage the Random number inside the ArrayList
-
             for (int value : arrayListXvALUE) {
                 Log.e("array list value is: ", "" + value);    //iterate through the ArrayList Value;
-
                 switch (color[value].getId()) {
                     case R.id.red:
-
                         handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 red.startAnimation(animation);
                             }
-                            //  Toast.makeText(getBaseContext(), "Flash: " + finalJ, Toast.LENGTH_SHORT).show();
                         }, 2000 * count);
                         count++;
-
-
                         break;
                     case R.id.blue:
-
-
                         handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 blue.startAnimation(animation);
                             }
-                            //  Toast.makeText(getBaseContext(), "Flash: " + finalJ, Toast.LENGTH_SHORT).show();
                         }, 2000 * count);
-
                         count++;
                         break;
                     case R.id.green:
-
-
                         handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 green.startAnimation(animation);
                             }
-                            //  Toast.makeText(getBaseContext(), "Flash: " + finalJ, Toast.LENGTH_SHORT).show();
                         }, 2000 * count);
                         count++;
-
                         break;
                     case R.id.yellow:
-
-
                         handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 yellow.startAnimation(animation);
                             }
-                            //  Toast.makeText(getBaseContext(), "Flash: " + finalJ, Toast.LENGTH_SHORT).show();
                         }, 2000 * count);
                         count++;
-
                         break;
-
                 }
-
-
-
-
-
             }
             running = false;
-            Log.e("running changed to", "" + running);
-            clickCheck(view);
+            Log.e("r change after while to", "" + running);
+            userChoiceList.clear();
         }
     }
-
-
     public void clickCheck(View view) {
-
         boolean test = false;
-
+        count=1;   //to rest the count to keep the Animation in good time  when ill back again to the simon for the next levels
         switch (view.getId()) {
             case R.id.red:
                 userChoiceList.add(0);
@@ -181,33 +138,31 @@ public class MainActivity extends AppCompatActivity {
             case R.id.green:
                 userChoiceList.add(2);
                 break;
-
             case R.id.yellow:
                 userChoiceList.add(3);
                 break;
         }
         Log.e("on check bee called", "now");
         Log.e("userChoiceLIST SIZE IS:", "" + userChoiceList.size());
-
         if (arrayListXvALUE.size() == userChoiceList.size()) {
             test = true;
-            Log.e("now both array are :",""+test);
-        }
-
-
-        if (test) {
-
-            if (arrayListXvALUE.equals(userChoiceList)) {
-                running=true;
-                simon(view);
-
-                Toast.makeText(this, "Great jobe , Click for Next", Toast.LENGTH_SHORT).show();
-            } else {
-                running=false;
-
-                Toast.makeText(this, "not good", Toast.LENGTH_SHORT).show();
+            Log.e(" both array same size :", "" + test);
+            if (test) {
+                Log.e(" test door open 2check:", "" + test);
+                if (arrayListXvALUE.equals(userChoiceList)) {
+                    Toast.makeText(this, "Great job , Next level: " + userChoiceList.size()+1, Toast.LENGTH_LONG).show();
+                    score.setText(""+userChoiceList.size());
+                    running = true;
+                    simon(view);
+                } else {
+                    running = false;
+                    Toast.makeText(this, "Not Match,Game over", Toast.LENGTH_LONG).show();
+                    play.setTag(1);
+                    play.setText("play");
+                    score.setText("");
+                    arrayListXvALUE.clear();
+                }
             }
         }
-
     }
 }
