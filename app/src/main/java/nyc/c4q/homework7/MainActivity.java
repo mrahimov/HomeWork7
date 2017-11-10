2c422b3cbbf749d64e8ef0f5713f6627215ad570
Murad Rahimov <rahimov.mu@gmail.com>
2017-11-10 12:32:31 -0500

    add Queue to stop game at first incorrect input

--- app/src/main/java/nyc/c4q/homework7/MainActivity.java
+++ app/src/main/java/nyc/c4q/homework7/MainActivity.java
@@ -1,224 +1,220 @@
 package nyc.c4q.homework7;
-
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton red;
+    private ImageButton green;
+    private ImageButton blue;
+    private ImageButton yellow;
     private Button play;
-    private ImageButton red, blue, green, yellow;
-    private TextView score, bestScore, level;
-    private ImageButton[] color;
-    private boolean running;
-    private int buttonTag;
     private int count = 1;
-    private final String scoreNow = "scoreNow";
-    private final String arrayListKey = "arrayListKey";
-    AlphaAnimation animation = new AlphaAnimation(1f, 0f);
-    Random random;
-    ArrayList<Integer> arrayListXvALUE;
-    ArrayList<Integer> userChoiceList;
+    private Score score;
+    private TextView bestScoreView;
+    private TextView levelView;
+    private int bestScore;
+    Animation animation = new AlphaAnimation(1f, 0f);
+    private static final String KEY_ARR = "arrayList";
+    private static final String TEMP_ARR = "temp";
+    private static final String KEY = "key";
+    private static final String SHARED_PREFERENCES = "savedScore";
+    private static final String KEY_BESTSCORE = "savedBestScore";
     private MediaPlayer gameOver;
     private MediaPlayer mpGame;
     private MediaPlayer sound;
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
-    String scoreBest = "";
-
-
+    Queue<Integer> queue = new LinkedList<>();
+    ArrayList<Integer> deletedNum = new ArrayList<>();
+    ArrayList<Integer> temp = new ArrayList<>();
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(KEY, score);
+        outState.putIntegerArrayList(KEY_ARR, deletedNum);
+        temp.addAll(queue);
+        outState.putIntegerArrayList(TEMP_ARR, temp);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupview();
-        gameOver = MediaPlayer.create(this, R.raw.gameover);
-        mpGame = MediaPlayer.create(this, R.raw.game);
-        sound = MediaPlayer.create(this, R.raw.sound);
-        color = new ImageButton[]{red, blue, green, yellow};        //storage the color inside the button
-        random = new Random();                                 //Generate Random number to access to the color
-        arrayListXvALUE = new ArrayList<>();                   //storage the color in the sequence to use them later .
-        userChoiceList = new ArrayList<>();                    //storage the user choice to compare later with arryListXValue;
-        animation.setDuration(100);
-
-        sharedPreferences = getSharedPreferences("simon_values", MODE_PRIVATE); //Acesss
-        editor = sharedPreferences.edit();  //Edit
-
-        scoreBest = sharedPreferences.getString("best_score", "0");//call back
-        bestScore.setText(scoreBest);
-
-        //to flash the button
-        if (savedInstanceState != null) {
-
-            score.setText(savedInstanceState.getString(scoreNow));
-            level.setText(savedInstanceState.getString("level"));
-            arrayListXvALUE = savedInstanceState.getIntegerArrayList(arrayListKey);
-
-        }
-    }
-
-    public void setupview() {
         red = (ImageButton) findViewById(R.id.red);
         blue = (ImageButton) findViewById(R.id.blue);
         green = (ImageButton) findViewById(R.id.green);
         yellow = (ImageButton) findViewById(R.id.yellow);
-        score = (TextView) findViewById(R.id.socre);
-        bestScore = (TextView) findViewById(R.id.bestscore);
-        level = (TextView) findViewById(R.id.level);
-        play = (Button) findViewById(R.id.play);                //to start play or to stop
-        play.setTag(1);
-    }
-
-    public void onplay(final View view) {
-        buttonTag = (int) play.getTag();
-        if (buttonTag == 1) {
-            play.setTag(2);
-            play.setText("Quit game");
-            level.setText("Level 1");
-            simon();
-            clickCheck(view);
-            score.setText("");
-        } else if (buttonTag == 2) {
-            play.setTag(1);
-            play.setText("play");
-            arrayListXvALUE.clear();
-            score.setText("");
-            Toast.makeText(getBaseContext(), "Game Finsh: ", Toast.LENGTH_SHORT).show();
+        play = (Button) findViewById(R.id.play);
+        levelView = (TextView) findViewById(R.id.socre_view);
+        bestScoreView = (TextView) findViewById(R.id.bestscore);
+        animation.setDuration(80);
+        bestScoreView.setText(String.valueOf(retrieveBestScore()));
+        gameOver = MediaPlayer.create(this, R.raw.gameover);
+        mpGame = MediaPlayer.create(this, R.raw.game);
+        sound = MediaPlayer.create(this, R.raw.sound);
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getParcelable(KEY);
+            levelView.setText("SCORE" + score.getValue());
+            deletedNum = savedInstanceState.getIntegerArrayList(KEY_ARR);
+            temp = savedInstanceState.getIntegerArrayList(TEMP_ARR);
+            queue.addAll(temp);
+        } else {
+            score = new Score(0);
         }
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                queue.clear();
+                deletedNum.clear();
+                randomNumber();
+            }
+        });
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.start();
+                int number = addToArrayList(red);
+                if (number != 0) {
+                    Toast.makeText(getApplicationContext(), "you lost!", Toast.LENGTH_LONG).show();
+                    saveBestSCore();
+                    score.setValue(0);
+                    gameOver.start();
+                } else if (queue.size() == 0) {
+                    startNewRound();
+                }
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.start();
+                int number = addToArrayList(blue);
+                if (number != 1) {
+                    Toast.makeText(getApplicationContext(), "you lost!", Toast.LENGTH_LONG).show();
+                    saveBestSCore();
+                    score.setValue(0);
+                    gameOver.start();
+                } else if (queue.size() == 0) {
+                    startNewRound();
+                }
+            }
+        });
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.start();
+                int number = addToArrayList(green);
+                if (number != 2) {
+                    Toast.makeText(getApplicationContext(), "you lost!", Toast.LENGTH_LONG).show();
+                    saveBestSCore();
+                    score.setValue(0);
+                    gameOver.start();
+                } else if (queue.size() == 0) {
+                    startNewRound();
+                }
+            }
+        });
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.start();
+                int number = addToArrayList(yellow);
+                if (number != 3) {
+                    Toast.makeText(getApplicationContext(), "you lost!", Toast.LENGTH_LONG).show();
+                    saveBestSCore();
+                    score.setValue(0);
+                    gameOver.start();
+                } else if (queue.size() == 0) {
+                    startNewRound();
+                }
+            }
+        });
     }
-
-    private void simon() {
-
-        running = true;
+    public void randomNumber() {
         Random r = new Random();
-        int next = r.nextInt(4);
-        arrayListXvALUE.add(next);
-
-        for (final Integer light : arrayListXvALUE) {
-            if (light == 0) {
-
+        int n = r.nextInt(4);
+        queue.add(n);
+        for (int i : queue) {
+            if (i == 0) {
+                count++;
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         red.startAnimation(animation);
                         mpGame.start();
                     }
-                }, 600 * count);
-
+                }, 500 * count);
+            } else if (i == 1) {
                 count++;
-            } else if (light == 1) {
-
+                mpGame.start();
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         blue.startAnimation(animation);
                         mpGame.start();
                     }
-                }, 600 * count);
+                }, 500 * count);
+            } else if (i == 2) {
                 count++;
-            } else if (light == 2) {
-
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         green.startAnimation(animation);
                         mpGame.start();
                     }
-                }, 600 * count);
-
+                }, 500 * count);
+            } else if (i == 3) {
                 count++;
-            } else if (light == 3) {
-
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         yellow.startAnimation(animation);
                         mpGame.start();
                     }
-                }, 600 * count);
-
-                count++;
+                }, 500 * count);
             }
-
         }
         count = 1;
-        running = false;
-        Log.e("r change after while to", "" + running);
-        userChoiceList.clear();
     }
-
-
-    public void clickCheck(View view) {
-        switch (view.getId()) {
-            case R.id.red:
-                userChoiceList.add(0);
-                sound.start();
-                break;
-            case R.id.blue:
-                userChoiceList.add(1);
-                sound.start();
-                break;
-            case R.id.green:
-                userChoiceList.add(2);
-                sound.start();
-                break;
-            case R.id.yellow:
-                userChoiceList.add(3);
-                sound.start();
-                break;
+    public void startNewRound() {
+        int x = score.getValue() + 1;
+        score.setValue(x);
+        levelView.setText("level " + x);
+        queue.addAll(deletedNum);
+        deletedNum.clear();
+        randomNumber();
+    }
+    public int addToArrayList(ImageButton button) {
+        button.startAnimation(animation);
+        int number = queue.poll();
+        deletedNum.add(number);
+        return number;
+    }
+    public void saveBestSCore() {
+        int currentScore = score.getValue();
+        if (currentScore > bestScore) {
+            bestScoreView.setText(String.valueOf(currentScore));
+            SharedPreferences prefs = getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt(KEY_BESTSCORE, currentScore);
+            editor.apply();
+            bestScore = currentScore;
         }
-        Log.e("on check bee called", "now");
-        Log.e("userChoiceLIST SIZE IS:", "" + userChoiceList.size());
-        if (arrayListXvALUE.size() == userChoiceList.size()) {
-            if (arrayListXvALUE.equals(userChoiceList)) {
-
-                Toast.makeText(this, "Great job , Next level: " + (userChoiceList.size() + 1), Toast.LENGTH_SHORT).show();
-                score.setText("" + userChoiceList.size());
-                level.setText("Level " + (userChoiceList.size() + 1));
-                running = true;
-                if (Integer.parseInt(bestScore.getText().toString()) < userChoiceList.size()) {
-                    editor.putString("best_score", (String.valueOf(userChoiceList.size()))).commit();
-                    bestScore.setText(String.valueOf(userChoiceList.size()));
-                }
-
-                simon();
-            } else {
-                quiteGame();
-            }
-        }
-
     }
-
-    private void quiteGame() {
-        running = false;
-        Toast.makeText(this, "Game Over Yeah\n(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧Rusi!", Toast.LENGTH_LONG).show();
-        gameOver.start();
-        play.setTag(1);
-        play.setText("play");
-        score.setText("");
-
-        arrayListXvALUE.clear();
-    }
-
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putString("scoreNow", score.getText().toString());
-        outState.putString("level", level.getText().toString());
-        outState.putIntegerArrayList("arrayListKey", arrayListXvALUE);
-        outState.putIntegerArrayList("userArr", userChoiceList);
-        super.onSaveInstanceState(outState);
+    public int retrieveBestScore() {
+        SharedPreferences preferences = getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        int retrievedScore = preferences.getInt(KEY_BESTSCORE, 0);
+        return retrievedScore;
     }
 }
